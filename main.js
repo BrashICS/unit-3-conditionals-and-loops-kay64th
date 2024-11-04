b0d720740aaeebfdf9db3952f75c0b0e7e49a0be
Khattab <khattab.al-jumaili1@stu.ocsb.ca>
2024-11-04 10:50:53 -0500

    ffd

--- main.js
+++ main.js
@@ -1,29 +1,78 @@
-/**
- * Unit 3 - Conditionals and Loops
- * ICS3 - Mr. Brash 🐿️
- * 
- * For this unit, Mr. Brash is trying something different. Each lesson will
- * be in this one repository, separated in the Lessons folder.
- * 
- * If you run into Git or Github issues, call Mr. Brash over.
- **/ 
+// Setup the menu
+let message = `Hi! Please make a selection:
+1 - Play
+2 - Options
+3 - DLC
+4 - Check for Updates
+5 - Exit
+`
+// Prompt with the menu
+let selection = Number(prompt(msg));
 
-
-'use strict';
-
-/*** Event Listeners ***/
-
-
-
-/*** Functions ***/
-
-// A very accurate rounding function
-function round(value, decimals) {
-    let multiplier = 10**decimals;
-    return Math.round((value + Number.EPSILON) * multiplier) / multiplier;
+if (selection == 1) {
+    play()
+}
+else if (selection == 2) {
+    Options()
+}
+else if (selection == 3) {
+    dlc()
+}
+else if (selection == 4) {
+    update()
+}
+else if (selection == 5) {
+    exit()
 }
 
-// Get a random number from min to max
-function randInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function startgame() 
+
+
+let name = prompt("Please enter your name:")
+    alert("Welcome, " + name + " to Haunted St. Matthew HS!");
+{
+    let choice = prompt("You can go to the (1) library, (2) basement or (3) autoshop.")
+
+    if (choice == 1) {
+        library()
+        alert("GAME OVER")
+        return;
+    }
+
+    if (choice == 2) {
+        basement()
+        alert("GAME OVER")
+        return;
+    }
+
+    if (choice == 3) {
+        autoshop()
+        alert("GAME OVER")
+        return;
+    }
+}
+function library() {
+let lib = prompt("You see a creepy old book on a dusty table. Do you (1) open it or (2) leave it alone?")
+}
+
+if (lib == 1) {
+lib = alert("A ghost emerges from the book and scares you to death!")
+}
+
+if (lib == 2) {
+lib = alert("You quietly leave the library and find the exit. You survive!")
+}
+
+function basement() {
+    let base = alert = ("you have made an incorrect selection... The haunted building closes in on you and you die a slow and VERY painful death.")
+}
+
+function autoshop() {
+    let auto = prompt("You hear strange noises. Do you (1) investigate or (2) run away?")
+}
+    if (auto == 1) {
+        auto = alert("You discover a car trunk filled with candy!")
+}
+if (auto == 2) {
+    auto = alert("You trip and fall... The ghost catches you!")
 }
